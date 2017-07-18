b0e3652a51085138e686cf59425b916495235a02
Matt Davis <jiffyclub@gmail.com>
2017-07-17 22:25:28 -0700

    misc updates to prep for Heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python -m app.app --port=$PORT --db_url=$DATABASE_URL
--- run_app_local.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-python -m app.app \
-    --tornado_log_file=./tornado.log \
-    --app_log_file=./app.log \
-    --db_file=./ipborg.db \
-    --public_salt=abcd \
-    --secret_salt=efgh
